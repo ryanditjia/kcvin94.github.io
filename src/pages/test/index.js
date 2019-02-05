d424a6b3024e4c09a5db1146dd87f391cfe4cf8c
Kevin Nguyen <kcvin94@gmail.com>
2019-02-05 22:27:29 +0900

    gatsby doesnt like style files in page components

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,5 +1,4 @@
 import React from "react"
 import { Link } from "gatsby";
-import { TestStyle } from './style';
 
-export default () => <div css={TestStyle}><Link to="/">Testsss</Link></div>
+export default () => <div><Link to="/">Testsss</Link></div>
--- src/pages/test/style.js
+++
@@ -1,6 +0,0 @@
-import { css } from '@emotion/core';
-
-export const TestStyle = css`
-  color: red;
-  font-weight: bold;
-`;
